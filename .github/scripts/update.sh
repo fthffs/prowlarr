7f247bc02c53ee7cce5e624042b601101f2f22af
fthffs <fthffs@pm.me>
2024-11-10 19:39:39 +0100

    initial commit

---
+++ .github/scripts/update.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+prowlarr() {
+  prowlarr_url="https://prowlarr.servarr.com/v1/update/master/changes?os=linux&runtime=netcore"
+  local new_version="$(curl -sSL "${prowlarr_url}" | jq '.[0].version' -r)"
+
+  if [ "${new_version}" ]; then
+    sed -i "s/PROWLARR_VERSION=.*/PROWLARR_VERSION=${new_version}/" prowlarr/Dockerfile
+  fi
+
+  if output=$(git status --porcelain) && [ -z "$output" ]; then
+    # working directory clean
+    echo "no new prowlarr version available!"
+  else
+    # uncommitted changes
+    git commit -a -m "updated prowlarr to version: ${new_version}"
+    git push
+  fi
+}
+
+prowlarr
